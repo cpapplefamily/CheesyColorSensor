2175a8f6cd96bcb72e45b45c637208bebf840dbb
Corey Applegate <cpapplefamily@gmail.com>
2022-08-12 00:00:49 -0500

    Non Blocking serial and parse Websocket

--- src/Python/NMRC_RapidReact_FMS_Communication.py
+++ src/Python/NMRC_RapidReact_FMS_Communication.py
@@ -1,3 +1,5 @@
+from ast import Global
+from locale import currency
 import os
 import serial
 import serial.tools.list_ports
@@ -5,15 +7,19 @@ import requests
 import time
 import _thread as thread
 import websocket
+import json
 
-FMS_IP = "10.0.100.05"
-#FMS_IP = "192.168.1.92"
+#FMS_IP = "10.0.100.05"
+FMS_IP = "192.168.1.187"
 FMS_PORT = "8080"
 FMS_SERVER = FMS_IP + ":" + FMS_PORT
 ALLIANCE_COLOR = 'red' # Change accordingly
 #ALLIANCE_COLOR = 'blue' # Change accordingly
 USERNAME = 'admin'
 PASSWORD = 'ProliantDL160'
+
+curent_matchState = 0
+last_matchState = 0
 
 goal_char_msg_map = {
     "S": '{ "type": "RU" }',
@@ -32,34 +38,68 @@ def find_arduino_port():
 # Retur na char recieved from arduino
 def get_goal_char(connection):
     connection.reset_input_buffer()
-    goal_char = connection.read(1).decode('UTF-8')
+    if (connection.inWaiting() > 0):
+        goal_char = connection.read(1).decode('UTF-8')
+    else:
+        goal_char = "0"
     return goal_char
 
 def get_msg_from_goal_char(goal_char):
     return goal_char_msg_map[goal_char]
 
+
 def get_on_ws_open_callback(connection):
+    
     def on_ws_open(ws):
         print("Connected to FMS")
 
         def run(*args):
+            global curent_matchState
+            global last_matchState
             while(True):
                 goal_char = get_goal_char(connection)
-                print(f'Info: recieved "{goal_char}"')
+                if(goal_char != "0"):
+                    print(f'Info: recieved "{goal_char}"')
 
-                if (goal_char in goal_char_msg_map):
-                    print(f'Info: sent {get_msg_from_goal_char(goal_char)}')
-                    ws.send(get_msg_from_goal_char(goal_char))
+                    if (goal_char in goal_char_msg_map):
+                        print(f'Info: sent {get_msg_from_goal_char(goal_char)}')
+                        ws.send(get_msg_from_goal_char(goal_char))
+                    else:
+                        print('Error: unknown char recieved')
                 else:
-                    print('Error: unknown char recieved')
-                #Give Sign of Life
-                print('Info sent to Arduino')
-                #connection.write("s")
+                    #Give Sign of Life
+                    #print('Info sent to Arduino')
+                    if(curent_matchState != last_matchState):
+                        print('MatchState Changed')
+                        last_matchState = curent_matchState
+                    #connection.write("s")
+                    #print("No data")
             
 
         thread.start_new_thread(run, ())
     
     return on_ws_open
+    
+def on_message(ws, message):
+    global curent_matchState
+    # and returns dict.
+    data = json.loads(message)
+
+    print("JSON string = ", data)
+    print()
+
+    
+    if(data['type'] == 'ping'):
+        print('is ping')
+        print("Curent MatchState: %s" % (curent_matchState))
+        print("Last MatchState: %s" % (last_matchState))
+
+    if(data['type'] == 'matchTime'):
+        print('is matchTime')
+        curent_matchState = data['data']['MatchState']
+        print(curent_matchState)
+
+
     
 def open_websocket(serial_connection):
     def reopen_websocket():
@@ -72,6 +112,7 @@ def open_websocket(serial_connection):
 
     ws = websocket.WebSocketApp(f'ws://{FMS_SERVER}/panels/scoring/{ALLIANCE_COLOR}/websocket'
         , on_open=get_on_ws_open_callback(serial_connection)
+        , on_message=on_message
         , on_close=reopen_websocket
         , cookie="; ".join(["%s=%s" %(i, j) for i, j in res.cookies.get_dict().items()])
     )
