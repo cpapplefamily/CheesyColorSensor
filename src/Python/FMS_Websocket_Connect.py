2a55c89e169ec2866393c3922b266f32d7d85fe5
Corey Applegate <cpapplefamily@gmail.com>
2024-06-23 16:38:25 -0500

    added BankedAmpNotes python

--- src/Python/FMS_Websocket_Connect.py
+++ src/Python/FMS_Websocket_Connect.py
@@ -7,19 +7,21 @@ import _thread as thread
 import websocket
 import json
 
-#FMS_IP = "10.0.100.05"
-FMS_IP = "192.168.1.187"
+FMS_IP = "10.0.100.05"
+#FMS_IP = "192.168.1.187"
 FMS_PORT = "8080"
 FMS_SERVER = FMS_IP + ":" + FMS_PORT
-ALLIANCE_COLOR = 'Red' # Change accordingly
-#ALLIANCE_COLOR = 'Blue' # Change accordingly
+#ALLIANCE_COLOR = 'Red' # Change accordingly
+ALLIANCE_COLOR = 'Blue' # Change accordingly
 USERNAME = 'admin'
 PASSWORD = 'ProliantDL160'
 
-isWindows = True
+isWindows = False #Set when Testing with out RaspberryPi
+usingArduino = True # When I Don't have an Arduino Connected
 updateArduino = False
 CoopActivated = False
-BankedAmpNotes = 0
+curent_bankedAmpNotes = 0
+last_bankedAmpNotes = 2 #this forces a message
 AmplifiedTimePostWindow = False
 curent_matchState = '9'
 last_matchState = '0'
@@ -47,7 +49,7 @@ matchState_char_msg_map = {
     "9": '29'
 }
 
-ampState_char_msg_map = {
+bankedAmpNotes_char_msg_map = {
     "0": '30',
     "1": '31',
     "2": '32'
@@ -92,6 +94,9 @@ def get_msg_from_goal_char(goal_char):
 def get_msg_from_MatchState_char(MatchStat_char):
     return matchState_char_msg_map[MatchStat_char]
 
+def get_msg_from_BankedAmpNotes_char(BankedAmpNotes_char):
+    return bankedAmpNotes_char_msg_map[BankedAmpNotes_char]
+
 def int_to_bytes(x: int) -> bytes:
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
 
@@ -103,7 +108,8 @@ def get_on_ws_open_callback(usb_connection):
             global curent_matchState
             global last_matchState
             global CoopActivated
-            global BankedAmpNotes
+            global curent_bankedAmpNotes
+            global last_bankedAmpNotes
             global AmplifiedTimePostWindow
             global AmplifiedTimeRemainingSec
             global updateArduino
@@ -121,8 +127,28 @@ def get_on_ws_open_callback(usb_connection):
                     #Give Sign of Life
                 else:
                     if(updateArduino):
-                        print('update Arduino')
+                        #print('update Arduino')
                         updateArduino = False
+                    #priority is match state
+                    if(curent_matchState != last_matchState):
+                        print('MatchState Changed')
+                        if (str(curent_matchState) in matchState_char_msg_map):
+                            print(f'Info: sent to Arduino {get_msg_from_MatchState_char(str(curent_matchState))}')
+                            message = get_msg_from_MatchState_char(str(curent_matchState)) + '\n'
+                            usb_connection.write(bytes(message, 'utf-8'))
+                        else:
+                            print('MatchState Error')
+                        last_matchState = curent_matchState
+                    #next priority
+                    elif(curent_bankedAmpNotes != last_bankedAmpNotes):
+                        print('bankedAmpNotes Changed')
+                        if (str(curent_bankedAmpNotes) in bankedAmpNotes_char_msg_map):
+                            print(f'Info: sent to Arduino {get_msg_from_BankedAmpNotes_char(str(curent_bankedAmpNotes))}')
+                            message = get_msg_from_BankedAmpNotes_char(str(curent_bankedAmpNotes)) + '\n'
+                            usb_connection.write(bytes(message, 'utf-8'))
+                        else:
+                            print('bankedAmpNotes Error')
+                        last_bankedAmpNotes = curent_bankedAmpNotes
             
 
         thread.start_new_thread(run, ())
@@ -134,7 +160,7 @@ def on_message(ws, message):
     global last_matchState
     global en_Serial_Print
     global CoopActivated
-    global BankedAmpNotes
+    global curent_bankedAmpNotes
     global AmplifiedTimePostWindow
     global AmplifiedTimeRemainingSec
     global updateArduino
@@ -148,10 +174,12 @@ def on_message(ws, message):
 
     if(data['type'] == 'ping'):
         if(en_Serial_Print):
-            print("JSON string = ", data)
+            #print("JSON string = ", data)
             print('is ping')
             print("Curent MatchState: %s" % (curent_matchState))
             print("Last MatchState: %s" % (last_matchState))
+            print("Curent BankedAmpNotes: %s" % (curent_bankedAmpNotes))
+            print("Last BankedAmpNotes: %s" % (last_bankedAmpNotes))
         updateArduino = True
 
     if(data['type'] == 'matchTime'):
@@ -168,7 +196,7 @@ def on_message(ws, message):
         print()
         curent_matchState = data['data']['MatchState']
         allianceScore = data['data'][ALLIANCE_COLOR]['Score']
-        BankedAmpNotes = allianceScore["AmpSpeaker"]["BankedAmpNotes"]
+        curent_bankedAmpNotes = allianceScore["AmpSpeaker"]["BankedAmpNotes"]
         CoopActivated = allianceScore["AmpSpeaker"]["CoopActivated"]
         AmplifiedTimePostWindow = data['data'][ALLIANCE_COLOR]["AmplifiedTimePostWindow"]
         AmplifiedTimeRemainingSec = data['data'][ALLIANCE_COLOR]["AmplifiedTimeRemainingSec"]
@@ -179,7 +207,7 @@ def on_message(ws, message):
             print('is realtimeScore')
             print("Curent MatchState: %s" % (curent_matchState))
             #print("allianceScore: %s" % (allianceScore))
-            print("BankedAmpNotes = ", BankedAmpNotes)
+            print("BankedAmpNotes = ", curent_BankedAmpNotes)
             print("CoopertitionStatus = ", CoopActivated)
             print("AmplifiedTimePostWindow = ", AmplifiedTimePostWindow)
             print("Amplification Sec Remaining = ",AmplifiedTimeRemainingSec)
@@ -221,7 +249,7 @@ def open_websocket2():
     ws.run_forever()
 
 def main():
-    usingArduino = True
+    global usingArduino
     """Loops until a connection is made to the Arduino.
     When connected advances to check for Area
     """
